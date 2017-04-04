b95fb0e6ca3f82bd14af33b2b6aed2d739001a23
hoangtd <hoangtd.ptd@gmail.com>
2017-04-04 09:26:11 +0700

    1st

---
+++ ServiceWithProxy/NetUtils.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Net.Utils
+{
+    public class MyWebClient : WebClient
+    {
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            HttpWebRequest request = base.GetWebRequest(address) as HttpWebRequest;
+            request.KeepAlive = false;
+            //request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+            return request;
+        }
+    }
+
+    public class ProxyConfig
+    {
+        public const string PROXY_CONFIG = "proxy.conf";
+
+        public bool NoneProxy { get; set; }
+        public bool UseIEProxy { get; set; }
+        public bool UseCustomProxy { get; set; }
+        public string ProxyHost { get; set; }
+        public int ProxyPort { get; set; }
+        public bool ProxyAuthen { get; set; }
+        public string ProxyUser { get; set; }
+        public string ProxyPass { get; set; }
+
+        //public static ProxyConfig GetConfig()
+        //{
+        //    string configPath = Path.Combine(Application.StartupPath, PROXY_CONFIG);
+        //    if (string.IsNullOrEmpty(configPath) || !File.Exists(configPath))
+        //        return null;
+
+        //    try
+        //    {
+        //        System.Xml.Serialization.XmlSerializer reader = new System.Xml.Serialization.XmlSerializer(typeof(ProxyConfig));
+        //        FileInfo fi = new FileInfo(configPath);
+        //        using (System.IO.StreamReader file = new System.IO.StreamReader(configPath))
+        //        {
+        //            ProxyConfig config = (ProxyConfig)reader.Deserialize(file);
+        //            return config;
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        log2File.LogExceptionToFile(ex);
+        //        return null;
+        //    }
+        //}
+        public static IWebProxy SetProxy(ProxyConfig config)
+        {
+            if (config == null)
+                return null;
+
+            IWebProxy proxy = null;
+            if (config.NoneProxy)
+            {
+                proxy = null;
+            }
+            else if (config.UseCustomProxy)
+            {
+                Uri newUri = new Uri(string.Format("http://{0}:{1}", config.ProxyHost, config.ProxyPort));
+                proxy = new WebProxy(newUri);
+                if (config.ProxyAuthen)
+                {
+                    proxy.Credentials = new NetworkCredential(config.ProxyUser, config.ProxyPass);
+                }
+            }
+            else if (config.UseIEProxy)
+            {
+                // use system proxy
+                proxy = WebRequest.GetSystemWebProxy();
+
+                // var request = (HttpWebRequest)WebRequest.Create(link);
+                //if (request.Proxy != null) // check system proxy
+                //{
+                //    var uri = request.Proxy.GetProxy(request.RequestUri);
+                //    proxy = new WebProxy(uri, false);
+                //    proxy.Credentials = System.Net.CredentialCache.DefaultCredentials;                    
+                //}               
+            }
+
+            // set global http request proxy
+            WebRequest.DefaultWebProxy = proxy;
+            return proxy;
+        }
+        //public static void SaveConfig(ProxyConfig config)
+        //{
+        //    string configPath = Path.Combine(Application.StartupPath, PROXY_CONFIG);
+        //    //if (!File.Exists(configPath))
+        //    //    File.Create(configPath).Close();
+
+        //    var writer = new System.Xml.Serialization.XmlSerializer(typeof(ProxyConfig));
+        //    using (var wfile = new System.IO.StreamWriter(configPath, false))
+        //    {
+        //        writer.Serialize(wfile, config);
+        //        wfile.Close();
+        //    }
+        //}
+
+
+    }
+}
---
+++ ServiceWithProxy/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceWithProxy.TestService;
+using System.Net;
+using System.ServiceModel;
+
+namespace ServiceWithProxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //init
+            string endpointUri = @"http://gw.softdreams.vn:6161/Services.svc";
+            bool useCredentials = false;
+            bool useDefaultWebProxy = true;
+
+            //custom proxy
+            CustomProxy customProxy = new CustomProxy();
+            customProxy.protocol = "http"; //or https
+            //pass
+            customProxy.host = "123.30.238.16";
+            //not pass
+            //customProxy.host = "171.251.51.218";
+            customProxy.port = "3128";
+
+            //setup proxy
+            string proxy = string.Format("{0}://{1}:{2}", customProxy.protocol, customProxy.host, customProxy.port);
+            BasicHttpBinding binding = new BasicHttpBinding();
+            binding.Security.Mode = BasicHttpSecurityMode.TransportCredentialOnly;
+
+            //use default web proxy or not
+            if (!useDefaultWebProxy)
+            {
+                binding.UseDefaultWebProxy = false;
+                binding.ProxyAddress = new Uri(proxy);
+            }
+            else
+            {
+                binding.UseDefaultWebProxy = true;
+            }
+            EndpointAddress endpoint = new EndpointAddress(endpointUri);
+            ServicesClient client = null;
+            if (!useCredentials)
+            {
+                binding.Security.Transport.ProxyCredentialType = HttpProxyCredentialType.None;
+                client = new ServicesClient(binding, endpoint);
+                client.ClientCredentials.Windows.ClientCredential = System.Net.CredentialCache.DefaultNetworkCredentials;
+            }
+            else
+            {
+                binding.Security.Transport.ProxyCredentialType = HttpProxyCredentialType.Basic;
+                client = new ServicesClient(binding, endpoint);
+                CustomCredential customCredential = new CustomCredential("", "", "");
+                client.ClientCredentials.Windows.ClientCredential = new NetworkCredential(customCredential.username, customCredential.password, customCredential.domain);
+            }
+
+            System.Net.ServicePointManager.Expect100Continue = false;
+
+            //call service
+            object[] responseData = null;
+            client.Process("token", "sd8888h11", "e10adc3949ba59abbe56e057f20f883e", "serviceId", "C101", "", new byte[1], ref responseData);
+            Console.WriteLine(responseData[0]);
+            Console.ReadLine();
+        }
+    }
+
+    class CustomCredential
+    {
+        public string username { set; get; }
+        public string password { set; get; }
+        public string domain { set; get; }
+
+        public CustomCredential() { }
+
+        public CustomCredential(string username, string password, string domain)
+        {
+            this.username = username;
+            this.password = password;
+            this.domain = domain;
+        }
+    }
+
+    class CustomProxy
+    {
+        public string protocol { set; get; }
+        public string host { set; get; }
+        public string port { set; get; }
+
+        public CustomProxy() { }
+
+        public CustomProxy(string protocol, string host, string port)
+        {
+            this.protocol = protocol;
+            this.host = host;
+            this.port = port;
+        }
+    }
+}
